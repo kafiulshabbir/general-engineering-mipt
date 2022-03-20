4a7fa17d6d538302ae6c66356004ef0b50ee12da
Kafi Ul Shabbir <kafiulshabbir@gmail.com>
2022-03-20 16:14:00 +0300

    3rd task primary addition2

--- lab-01-gpio_10-02-2022/task-3-read-light-led.py
+++ lab-01-gpio_10-02-2022/task-3-read-light-led.py
@@ -9,7 +9,7 @@ GPIO.setup(PIN_BLINK_LED, GPIO.OUT)
 GPIO.setup(PIN_READ_SWITCH, GPIO.IN)
 
 while True:
-	GPIO.output(PIN_BLINK_LED, GPIO.input(PIN_READ_SWITCH))
-    #The next line is not verified, whether RPi can read acurattely at infinite frequency
-    time.sleep(1)
+    GPIO.output(PIN_BLINK_LED, GPIO.input(PIN_READ_SWITCH))
+    time.sleep(1) #Theline is not verified, whether RPi can read acurattely at infinite frequency
+    
     
